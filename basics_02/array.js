4b526becd2f0d67f2bc580c782494eb1048b3fac
Mahmood Hussain <hussainmahmood952@gmail.com>
2025-01-03 22:39:37 +0500

    project bg changer add

---
+++ basics_02/array.js
@@ -0,0 +1,36 @@
+// array
+
+const myArr =[0,1,2,3,4,5]
+const myHeros =["walid","ustaad"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop() 
+// removes the last element from the array
+
+// myArr.unshift(9) // adds the element at the beginning of the array
+// myArr.shift() // removes the first element from the array
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join() // converts the array into a string
+
+
+
+
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+
+
+
